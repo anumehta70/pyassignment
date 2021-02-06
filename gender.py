ae94564fd1fc5cc4fb28a7f77f3b1ab034631853
Anish kumar <78554578+anumehta70@users.noreply.github.com>
2021-02-06 19:07:36 +0530

    Create gender.py

---
+++ gender.py
@@ -0,0 +1,7 @@
+a=input("enter your name: ")
+b=input("age: ")
+c=input("choose gender as male female or transgender: ")
+if c=="male" or c=="female" or c=="transgender":
+    print(a,"\t",b,"\t",c)
+else:
+    print("choose correct gender")
