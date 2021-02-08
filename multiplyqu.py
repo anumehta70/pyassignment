2e2c32fafe645af639cbc1ed613af939c782aec3
Anish kumar <78554578+anumehta70@users.noreply.github.com>
2021-02-08 18:21:30 +0530

    Update and rename problem4.py to multiplyqu.py

---
+++ multiplyqu.py
@@ -0,0 +1,8 @@
+a=int("2")
+b=int("4")
+c=int("6")
+f=0
+f=a+b
+q=0
+q=f*c
+print(q)
--- problem4.py
+++
@@ -1,9 +0,0 @@
-n=[]
-for i in range(0,5):
-    n.append(int(input(f"enter a {i+1} no.")))
-a=n[0]+n[3]
-b=a*n[4]
-if b>100:
-    print("greater than 100")
-else:
-    print("not greater than 100")
